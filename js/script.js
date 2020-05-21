c6c4beda729ffab80f4a8dadbc244f08f55733cf
Rogerio Amorim <rogerio.amorim@caixa.gov.br>
2020-05-21 10:28:47 -0300

    corrected error in degress calc

--- js/script.js
+++ js/script.js
@@ -16,10 +16,10 @@ const updateAnalogic = () => {
     const secondsDegree = (seconds / 60) * 360 + 90;
     secondsHand.style.transform = `rotate(${secondsDegree}deg`;
     const minutes = now.getMinutes();
-    const minutesDegree = (minutes / 60) * 360 + (seconds / 60) * 6 + 90;
+    const minutesDegree = (minutes / 60) * 360 + 90;
     minutesHand.style.transform = `rotate(${minutesDegree}deg`;
     const hours = now.getHours();
-    const hoursDegree = (hours / 12) * 360 + (minutes / 60) * 30 + 90;
+    const hoursDegree = (hours / 12) * 360 + 90;
     HoursHand.style.transform = `rotate(${hoursDegree}deg`;
 };
 
